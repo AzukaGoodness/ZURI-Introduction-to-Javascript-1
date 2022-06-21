9f1feec23bb1ad7ae53574d9d22301e35ee3a573
Goodness <azukachukwunonso@gmail.com>
2022-06-21 18:28:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Hard Coding Personal Information
+document.write("<h2>ABOUT ME</h2>");
+document.write("Name: Azuka Goodness Chukwunonso");
+document.write("<br>");
+document.write("<br>");
+document.write("Height: 5'9");
+document.write("<br>");
+document.write("<br>");
+document.write("Country: Nigerian");
+
+
+ 
+
+
